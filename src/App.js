182e56cc79136da99046595e9865e3dd9355cb1b
Sergey Ganous <sergeyganous@gmail.com>
2021-02-21 13:10:46 -0800

    Add Sidebar Confiuration
    * Added sidebar and sidebar option components.
    * Added Material-ui packages for icons.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
+import Sidebar from './Sidebar';
 
 function App() {
   return (
     <div className="app">
     <h1>Hello World!</h1>
+
+    {/* Sidebar */}
+    <Sidebar />
+
+    {/* Feed */}
+
+    {/* Widget */}
     </div>
   );
 }
