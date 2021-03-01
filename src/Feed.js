d1503c187e30d752f213fb0925d9fa13292e34f3
Sergey Ganous <sergeyganous@gmail.com>
2021-02-28 21:32:33 -0800

    Add Tweet Support
    * Added support for submitting a tweet and posting it to the feed.
    * Added an animation library to display new tweets in the feed in a more
    dynamic manner.

--- src/Feed.js
+++ src/Feed.js
@@ -3,6 +3,7 @@ import './Feed.css';
 import TweetBox from './TweetBox';
 import Post from './Post';
 import db from './firebase';
+import FlipMove from 'react-flip-move';
 
 function Feed() {
   const [posts, setPosts] = useState([]);
@@ -22,16 +23,19 @@ function Feed() {
 
     <TweetBox />
 
-    {posts.map(post =>
-      <Post
-        displayName={post.displayName}
-        username={post.username}
-        verified={post.verified}
-        text={post.text}
-        image={post.image}
-        avatar={post.image}
-      />
-    )}
+    <FlipMove>
+      {posts.map(post =>
+        <Post
+          key={post.text}
+          displayName={post.displayName}
+          username={post.username}
+          verified={post.verified}
+          text={post.text}
+          image={post.image}
+          avatar={post.avatar}
+        />
+      )}
+    </FlipMove>
     </div>
 
   )
--- src/TweetBox.js
+++ src/TweetBox.js
@@ -1,21 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Tweetbox.css';
 import { Avatar, Button } from '@material-ui/core';
+import db from './firebase';
 
 function TweetBox() {
+  const [tweetMessage, setTweetMessage] = useState("");
+  const [tweetImage, setTweetImage] = useState("");
+
+  const sendTweet = e => {
+    e.preventDefault();
+    db.collection('Posts').add({
+      displayName: 'Serge',
+      username: 'sergencoffee',
+      verified:true,
+      text: tweetMessage,
+      image: tweetImage,
+      avatar: "https://sergeneeds.coffee/content/images/2020/12/PXL_20201218_224154058.jpg"
+    });
+
+    setTweetMessage("");
+    setTweetImage("");
+  };
+
   return (
     <div className="tweetBox">
       <form>
         <div className="tweetBox__input">
           <Avatar src="https://sergeneeds.coffee/content/images/2020/12/PXL_20201218_224154058.jpg"/>
-          <input placeholder="What's happening?" type="text"/>
+          <input
+            onChange={(e) => setTweetMessage(e.target.value)}
+            value={tweetMessage}
+            placeholder="What's happening?"
+            type="text"/>
         </div>
         <input
+          onChange={(e) => setTweetImage(e.target.value)}
+          value={tweetImage}
           className="tweetBox__inputImage"
           placeholder="Optional: Enter image URL"
           type="text"
         />
-      <Button className="tweetBox__tweetButton">Tweet</Button>
+      <Button
+        onClick={sendTweet}
+        type="submit"
+        className="tweetBox__tweetButton">Tweet</Button>
       </form>
     </div>
   )
